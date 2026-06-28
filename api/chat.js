690bc68a92f59aff1f92d54f08b97280f16e4ba2
WillCO8 <Willchonortega@gmail.com>
2026-06-28 03:00:23 -0400

    fix: switch to commonjs module exports

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // 1. Only allow POST requests (like sending a message)
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
